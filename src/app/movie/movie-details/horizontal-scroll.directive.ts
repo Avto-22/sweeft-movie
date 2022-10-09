9190b1e2bfbe4fa57cdae963d3ad480e00a18e86
Avto-22 <avtandilkinchikidze@gmail.com>
2022-10-09 19:41:47 +0400

    cast's div drag scroll

--- src/app/movie/movie-details/horizontal-scroll.directive.ts
+++ src/app/movie/movie-details/horizontal-scroll.directive.ts
@@ -1,13 +1,52 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: "[HorizontalScroll]",
   })
   export class HorizontalScrollDirective {
-    constructor(private element: ElementRef) {}
+
+    startX;
+    isDown:boolean = false;
+    scrollLeft;
+
+    constructor(
+      private element: ElementRef,
+      private renderer:Renderer2
+      ) {}
   
     @HostListener("wheel", ["$event"])
     onScroll(event: WheelEvent) {
       this.element.nativeElement.scrollLeft += event.deltaY;
+    }
+
+    @HostListener('mousedown', ['$event'])
+    onMouseDown(event){
+      this.renderer.addClass(this.element.nativeElement, 'active-cast');
+      this.isDown = true;
+      this.startX = event.pageX - this.element.nativeElement.offsetLeft;
+      this.scrollLeft = this.element.nativeElement.scrollLeft;
+    }
+
+    @HostListener('mouseleave')
+    onMouseLeave(){
+      this.isDown = false;
+      this.renderer.removeClass(this.element.nativeElement, 'active-cast');
+    }
+
+    @HostListener('mouseup')
+    onMouseUp(){
+      this.isDown = false;
+      this.renderer.removeClass(this.element.nativeElement, 'active-cast');
+    }
+
+    @HostListener('mousemove', ["$event"])
+    onMouseMove(event){
+      if(!this.isDown){
+        return;
+      }
+      event.preventDefault();
+      let x = event.pageX - this.element.nativeElement.offsetLeft;
+      let walk = (x - this.startX) * 1;
+      this.element.nativeElement.scrollLeft = this.scrollLeft - walk;
     }
   }
